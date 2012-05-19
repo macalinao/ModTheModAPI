34569e66d67acd5cf0d65ecb3e50c79d4768665d
Ian Macalinao <ianmacalinao@gmail.com>
2012-05-19 16:33:26 -0500

    Eventing stuff.

--- src/main/java/com/modthemod/api/event/Event.java
+++ src/main/java/com/modthemod/api/event/Event.java
@@ -1,13 +1,72 @@
 package com.modthemod.api.event;
 
+import java.util.Map;
+
+import com.modthemod.api.property.Property;
+
 /**
  * Represents an event, or something that has happened in the game.
  */
-public interface Event {
+public final class Event {
+	/**
+	 * The {@link EventType} of the {@link Event}.
+	 */
+	private final EventType type;
+
+	/**
+	 * The {@link Subject} of the {@link Event}.
+	 */
+	private final Subject subject;
+
+	/**
+	 * The properties of the {@link Event}.
+	 */
+	private final Map<String, Property<?>> properties;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param type
+	 *            The {@link EventType} of the event.
+	 * @param subject
+	 *            The {@link Subject} of the event.
+	 * @param properties
+	 *            The properties of the event.
+	 */
+	public Event(EventType type, Subject subject,
+			Map<String, Property<?>> properties) {
+		super();
+		this.type = type;
+		this.subject = subject;
+		this.properties = properties;
+	}
+
+	/**
+	 * Gets the {@link EventType} of the {@link Event}.
+	 * 
+	 * @return The {@link EventType} of the {@link Event}.
+	 */
+	public EventType getType() {
+		return type;
+	}
+
 	/**
 	 * Gets the subject of the event.
 	 * 
 	 * @return The subject of the event.
 	 */
-	public Subject getSubject();
+	public Subject getSubject() {
+		return subject;
+	}
+
+	/**
+	 * Gets a {@link Property} of the {@link Event}.
+	 * 
+	 * @param name
+	 *            The name of the {@link Property}.
+	 * @return The {@link Property}.
+	 */
+	public Property<?> getProperty(String name) {
+		return properties.get(name);
+	}
 }
--- src/main/java/com/modthemod/api/event/EventType.java
+++ src/main/java/com/modthemod/api/event/EventType.java
@@ -1,29 +1,78 @@
 package com.modthemod.api.event;
 
+import java.util.Map;
+
 import com.modthemod.api.mod.Mod;
+import com.modthemod.api.property.Property;
 
 /**
  * Represents a type of event.
  */
-public interface EventType {
+public final class EventType {
+	/**
+	 * The name of the {@link EventType}.
+	 */
+	private final String name;
+
+	/**
+	 * The {@link Mod} of the {@link EventType}.
+	 */
+	private final Mod mod;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param name
+	 *            The name of the {@link EventType}.
+	 * @param mod
+	 *            The {@link Mod} corresponding with this {@link EventType}.
+	 */
+	public EventType(String name, Mod mod) {
+		super();
+		this.name = name;
+		this.mod = mod;
+	}
+
 	/**
 	 * Gets the name of the event.
 	 * 
 	 * @return The name of the event.
 	 */
-	public String getName();
+	public String getName() {
+		return name;
+	}
 
 	/**
 	 * Gets the mod that this event is associated with.
 	 * 
 	 * @return The mod the event is associated with.
 	 */
-	public Mod getMod();
-	
+	public Mod getMod() {
+		return mod;
+	}
+
 	/**
 	 * Adds an {@link EventListener} to the event.
 	 * 
-	 * @param listener The {@link EventListener} to add.
+	 * @param listener
+	 *            The {@link EventListener} to add.
 	 */
-	public void addListener(EventListener listener);
+	public void addListener(EventListener listener) {
+		// TODO
+	}
+
+	/**
+	 * Creates an {@link Event} with this {@link EventType}.
+	 * 
+	 * @param subject
+	 *            The {@link Subject} of the {@link Event}.
+	 * @param properties
+	 *            The properties of the {@link Event}.
+	 * @return The created {@link Event}.
+	 */
+	public Event createEvent(Subject subject,
+			Map<String, Property<?>> properties) {
+		return new Event(this, subject, properties); // TODO should this call
+														// the event?
+	}
 }
