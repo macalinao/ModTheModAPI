d953f03929a9890cb28c88669ccead60c09f6b45
Ian Macalinao <ianmacalinao@gmail.com>
2012-05-20 18:37:54 -0500

    Added more stuff to Mod.

--- src/main/java/com/modthemod/api/mod/Mod.java
+++ src/main/java/com/modthemod/api/mod/Mod.java
@@ -1,21 +1,36 @@
 package com.modthemod.api.mod;
 
+import java.util.logging.Logger;
+
 /**
  * Represents a mod.
  */
-public abstract class Mod {
-	private final String name;
-
-	protected Mod(String name) {
-		this.name = name;
-	}
+public interface Mod {
+	/**
+	 * Gets the name of this {@link Mod}.
+	 * 
+	 * @return The name of this {@link Mod}.
+	 */
+	public String getName();
 
 	/**
-	 * Gets the name of the mod.
+	 * Gets the {@link ModDescription} of this mod.
 	 * 
-	 * @return The name of the mod.
+	 * @return The {@link ModDescription} of this mod.
 	 */
-	public String getName() {
-		return name;
-	}
+	public ModDescription getDescription();
+
+	/**
+	 * Gets the {@link Logger} for this mod.
+	 * 
+	 * @return The {@link Logger} for this mod.
+	 */
+	public Logger getLogger();
+
+	/**
+	 * Gets the {@link ModLoader} that loaded this {@link Mod}.
+	 * 
+	 * @return The {@link ModLoader} that loaded this {@link Mod}.
+	 */
+	public ModLoader getModLoader();
 }
