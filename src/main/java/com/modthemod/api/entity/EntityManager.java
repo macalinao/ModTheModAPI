cdeb8d604e7ed19490b2f3d1b09348b2822186d7
Ian Macalinao <ianmacalinao@gmail.com>
2012-05-23 19:37:11 -0500

    This doesn't work out.

--- src/main/java/com/modthemod/api/entity/EntityManager.java
+++ src/main/java/com/modthemod/api/entity/EntityManager.java
@@ -6,15 +6,5 @@ import com.modthemod.api.base.Base;
  * Manages entities.
  */
 public interface EntityManager {
-	/**
-	 * Instantiates an {@link Entity}.
-	 * 
-	 * @param base
-	 *            The base of the {@link Entity}.
-	 * @param args
-	 *            The arguments of the {@link Entity}.
-	 * @return The instantiated {@link Entity}.
-	 */
-	public Entity instantiateEntity(Base base, Object... args);
 
 }
--- src/main/java/com/modthemod/api/mod/ModDescription.java
+++ src/main/java/com/modthemod/api/mod/ModDescription.java
@@ -2,7 +2,6 @@ package com.modthemod.api.mod;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import com.google.gson.Gson;
